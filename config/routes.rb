723eaf2dc0d2da7c39707e0f88be44d5e949d6d3
toshi-1009 <dearstage0429@gmail.com>
2021-03-16 16:02:57 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   resources :users, only: [:edit, :update,]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create]
   root to: "messages#index"
 end
